ee91451cd294118dfd231b5cfa9b4399af2ebed1
kazdreamm <adil_1717@mail.ru>
2025-01-27 09:37:31 +0500

    Adil

--- Electronics/src/model/Product.java
+++ Electronics/src/model/Product.java
@@ -52,4 +52,14 @@ public class Product {
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
+    
+    @Override
+    public String toString() {
+        return "Product ID: " + id +
+                ", Name: " + name +
+                ", Category: " + category +
+                ", Price: $" + price +
+                ", Quantity: " + quantity +
+                ", Description: " + description;
+    }
 }
