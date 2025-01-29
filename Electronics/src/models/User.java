f03161cd4b8bf5ecb6ab0ad726d77a5ab1200beb
RAKHMAN SEIDIGALI <242171@astanait.edu.kz>
2025-01-29 23:12:54 +0500

    Update User.java

--- Electronics/src/models/User.java
+++ Electronics/src/models/User.java
@@ -1,23 +1,22 @@
 package models;
 
 public class User {
-    private int id;
+    private int id;  // ID пользователя
+    private String email;
+    private String password;
     private String name;
-    private String surname;
-    private boolean gender;
 
-    public User() {
+    public User(int id, String email, String password, String name) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
     }
 
-    public User(String name, String surname, boolean gender) {
-        setName(name);
-        setSurname(surname);
-        setGender(gender);
-    }
-
-    public User(int id, String name, String surname, boolean gender) {
-        this(name, surname, gender);
-        setId(id);
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
     }
 
     public int getId() {
@@ -28,37 +27,27 @@ public class User {
         this.id = id;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public boolean getGender() {
-        return gender;
-    }
-
-    public void setGender(boolean gender) {
-        this.gender = gender;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", gender=" + gender +
-                '}';
     }
 }
