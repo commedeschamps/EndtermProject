d9f9771f561757bcf5a177384de91b6a450d2dab
RAKHMAN SEIDIGALI <242171@astanait.edu.kz>
2025-01-29 23:17:13 +0500

    Update IOrderController.java

--- Electronics/src/controllers/interfaces/IOrderController.java
+++ Electronics/src/controllers/interfaces/IOrderController.java
@@ -1,7 +1,7 @@
 package controllers.interfaces;
 
 public interface IOrderController {
-    String createOrder(int userId, double totalAmount);
+    String createOrder(int userId, double totalAmount, String deliveryMethod, String paymentMethod);
     String getOrderById(int id);
-    String getAllOrders(); 
+    String getAllOrders();
 }
