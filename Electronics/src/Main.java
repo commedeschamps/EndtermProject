ac15dca266c90c2c1850132105cc8a8c4ad2663f
RAKHMAN SEIDIGALI <242171@astanait.edu.kz>
2025-01-29 22:44:43 +0500

    Update Main.java

--- Electronics/src/Main.java
+++ Electronics/src/Main.java
@@ -4,8 +4,6 @@ import data.PostgresDB;
 import data.interfaceces.IDB;
 import repositories.*;
 import repositories.interfaces.*;
-import java.util.Date;
-
 
 public class Main {
     public static void main(String[] args) {
@@ -14,20 +12,17 @@ public class Main {
         IUserRepository userRepository = new UserRepository(db);
         IProductRepository productRepository = new ProductRepository(db);
         IOrderRepository orderRepository = new OrderRepository(db);
-        IOrderItemRepository orderItemRepository = new OrderItemRepository(db);
         ICartRepository cartRepository = new CartRepository(db);
 
         IUserController userController = new UserController(userRepository);
         IProductController productController = new ProductController(productRepository);
         IOrderController orderController = new OrderController(orderRepository);
-        IOrderItemController orderItemController = new OrderItemController(orderItemRepository);
         ICartController cartController = new CartController(cartRepository);
 
         ShopApplication app = new ShopApplication(
                 userController,
                 productController,
                 orderController,
-                orderItemController,
                 cartController
         );
 
