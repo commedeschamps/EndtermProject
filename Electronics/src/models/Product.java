481f439bceebf71502feaee9470f401bec9efc6a
kazdreamm <adil_1717@mail.ru>
2025-01-29 19:46:40 +0500

    Adil

--- Electronics/src/models/Product.java
+++ Electronics/src/models/Product.java
@@ -8,7 +8,16 @@ public class Product {
     private int quantity;
     private String category;
 
-    public Product(String name, String description, double price, int quantityб, String category) {
+    public Product(int id, String name, String description, double price, int quantity, String category) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+    }
+    
+    public Product(String name, String description, double price, int quantity, String category) {
         this.name = name;
         this.description = description;
         this.price = price;
