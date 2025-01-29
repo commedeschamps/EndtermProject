cb72daeabbce1c2011591d9b7bc5d5c4dbe07a23
RAKHMAN SEIDIGALI <242171@astanait.edu.kz>
2025-01-29 23:14:12 +0500

    Update IUserRepository.java

--- Electronics/src/repositories/interfaces/IUserRepository.java
+++ Electronics/src/repositories/interfaces/IUserRepository.java
@@ -6,6 +6,5 @@ import java.util.List;
 
 public interface IUserRepository {
     boolean createUser(User user);
-    User getUserById(int id);
-    List<User> getAllUsers();
+    User getUserByEmail(String email);
 }
