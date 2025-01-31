bde358fe59f91b9aade5eaa4873bb7158eb3f3df
YermakhanKhamitov <242545@astanait.edu.kz>
2025-01-31 17:36:47 +0500

    Update User.java

--- Electronics/src/models/User.java
+++ Electronics/src/models/User.java
@@ -1,22 +1,38 @@
 package models;
 
 public class User {
-    private int id; 
+    private int id;
+    private String name;
+    private String surname;
+    private boolean gender;
     private String email;
     private String password;
-    private String name;
+    private double balance;
 
-    public User(int id, String email, String password, String name) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.name = name;
+    public User() {
     }
 
-    public User(String email, String password, String name) {
+    public User(String name, String surname, boolean gender, String email, String password, double balance) {
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
         this.email = email;
         this.password = password;
+        this.balance = balance;
+    }
+
+    public User(String name, String surname, boolean gender) {
         this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+        this.email = "";
+        this.password = "";
+        this.balance = 0.0;
+    }
+
+    public User(int id, String name, String surname, boolean gender, String email, String password, double balance) {
+        this(name, surname, gender, email, password, balance);
+        this.id = id;
     }
 
     public int getId() {
@@ -25,6 +41,30 @@ public class User {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
     }
 
     public String getEmail() {
@@ -43,11 +83,38 @@ public class User {
         this.password = password;
     }
 
-    public String getName() {
-        return name;
+    public double getBalance() {
+        return balance;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            this.balance += amount;
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && this.balance >= amount) {
+            this.balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", gender=" + gender +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", balance=" + balance +
+                '}';
     }
 }
