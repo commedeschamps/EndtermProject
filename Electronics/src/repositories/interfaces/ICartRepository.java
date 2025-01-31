9769ef94390e7eb3f1ebfa8c752168b6540f6f05
YermakhanKhamitov <242545@astanait.edu.kz>
2025-01-31 17:37:37 +0500

    Update ICartRepository.java

--- Electronics/src/repositories/interfaces/ICartRepository.java
+++ Electronics/src/repositories/interfaces/ICartRepository.java
@@ -4,7 +4,9 @@ import models.Cart;
 import models.OrderItem;
 
 public interface ICartRepository {
-    boolean addItemToCart(int userId, OrderItem item);
+    boolean addItemToCart(int userId, int productId, int quantity, double price);
     Cart getCartByUserId(int userId);
     boolean clearCart(int userId);
+    boolean removeItemFromCart(int userId, int productId);
+    boolean updateCartItemQuantity(int userId, int productId, int newQuantity);
 }
