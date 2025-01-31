a03879c62efc055b69d916644c33142ff48e9efa
YermakhanKhamitov <242545@astanait.edu.kz>
2025-01-31 17:35:35 +0500

    Update ICartController.java

--- Electronics/src/controllers/interfaces/ICartController.java
+++ Electronics/src/controllers/interfaces/ICartController.java
@@ -2,6 +2,9 @@ package controllers.interfaces;
 
 public interface ICartController {
     String addItemToCart(int userId, int productId, int quantity, double price);
+    String removeItemFromCart(int userId, int productId);
     String getCartByUserId(int userId);
     String clearCart(int userId);
+    String updateItemQuantity(int userId, int productId, int newQuantity);
+    double calculateTotalPrice(int userId);
 }
