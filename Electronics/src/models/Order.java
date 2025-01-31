f6a487aceb91b7928690a6dc0445e11ad8995ceb
YermakhanKhamitov <242545@astanait.edu.kz>
2025-01-31 17:46:09 +0500

    Update Order.java

--- Electronics/src/models/Order.java
+++ Electronics/src/models/Order.java
@@ -1,21 +1,21 @@
 package models;
 
-import java.util.Date;
+import java.util.Date; 
 
 public class Order {
     private int id;
     private int userId;
-    private Date orderDate;
     private double totalAmount;
-    private String deliveryMethod; // choice delivery or pick-up
-    private String paymentMethod;  //Cash, credit card, nfc
+    private Date orderDate;
+    private String shippingAddress;
+    private String billingAddress;
 
-    public Order(int userId, Date orderDate, double totalAmount, String deliveryMethod, String paymentMethod) {
+    public Order(int userId, double totalAmount, Date orderDate, String shippingAddress, String billingAddress) {
         this.userId = userId;
-        this.orderDate = orderDate;
         this.totalAmount = totalAmount;
-        this.deliveryMethod = deliveryMethod;
-        this.paymentMethod = paymentMethod;
+        this.orderDate = orderDate;
+        this.shippingAddress = shippingAddress;
+        this.billingAddress = billingAddress;
     }
 
     public Order(int id, int userId, Date orderDate, double totalAmount) {
@@ -23,41 +23,68 @@ public class Order {
         this.userId = userId;
         this.orderDate = orderDate;
         this.totalAmount = totalAmount;
-        this.deliveryMethod = deliveryMethod;
-        this.paymentMethod = paymentMethod;
+    }
+
+    public Order(int userId, double totalAmount, String orderDate, String shippingAddress, String billingAddress) {
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public int getUserId() {
         return userId;
     }
 
-    public Date getOrderDate() {
-        return orderDate;
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 
     public double getTotalAmount() {
         return totalAmount;
     }
 
-    public String getDeliveryMethod() {
-        return deliveryMethod;
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
     }
 
-    public String getPaymentMethod() {
-        return paymentMethod;
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public String getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
     }
 
     @Override
     public String toString() {
-        return "Order ID: " + id +
-                "\nUser ID: " + userId +
-                "\nOrder Date: " + orderDate +
-                "\nTotal Amount: " + totalAmount +
-                "\nDelivery Method: " + deliveryMethod +
-                "\nPayment Method: " + paymentMethod;
+        return "Order{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", totalAmount=" + totalAmount +
+                ", orderDate=" + orderDate +
+                ", shippingAddress='" + shippingAddress + '\'' +
+                ", billingAddress='" + billingAddress + '\'' +
+                '}';
     }
 }
