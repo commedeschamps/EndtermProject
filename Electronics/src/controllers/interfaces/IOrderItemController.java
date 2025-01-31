c543accb6acbfc8caa9a94c43945279819129458
YermakhanKhamitov <242545@astanait.edu.kz>
2025-01-31 17:36:11 +0500

    Update IOrderItemController.java

--- Electronics/src/controllers/interfaces/IOrderItemController.java
+++ Electronics/src/controllers/interfaces/IOrderItemController.java
@@ -4,4 +4,5 @@ public interface IOrderItemController {
     String createOrderItem(int orderId, int productId, int quantity, double price);
     String getOrderItemById(int id);
     String getAllOrderItems();
+    double calculateOrderItemTotalPrice(int quantity, double price);
 }
