104aa39b940dbbc33642f1504aa043a1b1a76320
YermakhanKhamitov <242545@astanait.edu.kz>
2025-02-07 00:41:15 +0500

    Update OrderItemController.java

--- Electronics/src/controllers/OrderItemController.java
+++ Electronics/src/controllers/OrderItemController.java
@@ -5,6 +5,7 @@ import models.OrderItem;
 import repositories.interfaces.IOrderItemRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OrderItemController implements IOrderItemController {
     private final IOrderItemRepository repo;
@@ -15,30 +16,26 @@ public class OrderItemController implements IOrderItemController {
 
     @Override
     public String createOrderItem(int orderId, int productId, int quantity, double price) {
-        OrderItem orderItem = new OrderItem(orderId, productId, quantity, price);
-        boolean created = repo.createOrderItem(orderItem);
-        return (created) ? "OrderItem was created" : "OrderItem creation failed";
+        return repo.createOrderItem(new OrderItem(orderId, productId, quantity, price))
+                ? "OrderItem was created" : "OrderItem creation failed";
     }
 
     @Override
     public String getOrderItemById(int id) {
-        OrderItem orderItem = repo.getOrderItemById(id);
-        return (orderItem == null) ? "OrderItem not found" : orderItem.toString();
+        return id <= 0 ? "Invalid OrderItem ID." :
+                repo.getOrderItemById(id) != null ? repo.getOrderItemById(id).toString() : "OrderItem not found";
     }
 
     @Override
     public String getAllOrderItems() {
-        List<OrderItem> orderItems = repo.getAllOrderItems();
-        StringBuilder response = new StringBuilder();
-        for (OrderItem item : orderItems) {
-            response.append(item.toString()).append("\n");
-        }
-        return response.toString();
+        return repo.getAllOrderItems().stream()
+                .map(OrderItem::toString)
+                .collect(Collectors.collectingAndThen(Collectors.joining("\n"),
+                        result -> result.isEmpty() ? "No OrderItems available." : result));
     }
 
     @Override
     public double calculateOrderItemTotalPrice(int quantity, double price) {
         return quantity * price;
     }
-
 }
