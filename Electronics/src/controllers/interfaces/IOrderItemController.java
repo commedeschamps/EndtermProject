5ef6b69aeeba00c16723c03cee96802f473a6021
RAKHMAN SEIDIGALI <242171@astanait.edu.kz>
2025-02-06 15:58:08 +0500

    rakhman

--- Electronics/src/controllers/interfaces/IOrderItemController.java
+++
@@ -1,8 +0,0 @@
-package controllers.interfaces;
-
-public interface IOrderItemController {
-    String createOrderItem(int orderId, int productId, int quantity, double price);
-    String getOrderItemById(int id);
-    String getAllOrderItems();
-    double calculateOrderItemTotalPrice(int quantity, double price);
-}
