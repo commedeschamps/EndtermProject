c719c4321d5f1abe5a3493488702150a03a67d30
RAKHMAN SEIDIGALI <242171@astanait.edu.kz>
2025-02-13 21:39:47 +0500

    Update ProductFactory.java

--- Electronics/src/factories/ProductFactory.java
+++ Electronics/src/factories/ProductFactory.java
@@ -1,25 +1,21 @@
 package factories;
 
 import models.Product;
+import models.ProductCreationRequest;
 
 public class ProductFactory implements Factory<Product> {
+
     @Override
-    public Product create(Object... args) {
-        if (args.length < 6) {
-            throw new IllegalArgumentException("Invalid arguments for Product creation");
-        }
+    public Product create(Object request) {
+        if (request instanceof ProductCreationRequest) {
+            ProductCreationRequest req = (ProductCreationRequest) request;
 
-        try {
-            Integer id = Integer.parseInt(args[0].toString());
-            String name = args[1].toString();
-            String description = args[2].toString();
-            Double price = Double.parseDouble(args[3].toString());
-            Integer quantity = Integer.parseInt(args[4].toString());
-            String category = args[5].toString();
+            int id = req.getId().intValue();
+            double price = req.getPrice().doubleValue();
 
-            return new Product(id, name, description, price, quantity, category);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Error converting arguments for Product creation: " + e.getMessage());
+            return new Product(id, req.getName(), req.getDescription(), price, req.getQuantity(), req.getCategory());
+        } else {
+            throw new IllegalArgumentException("Invalid request type");
         }
     }
 }
